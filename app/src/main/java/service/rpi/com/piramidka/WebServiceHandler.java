e063edd8738556dbc47d1116ab35825aa5b66059
pawbla <blachut.pawel@gmail.com>
2018-09-27 09:59:58 +0200

    small fixes

--- app/src/main/java/service/rpi/com/piramidka/WebServiceHandler.java
+++ app/src/main/java/service/rpi/com/piramidka/WebServiceHandler.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -34,20 +38,38 @@ public class WebServiceHandler extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... params) {
         Log.d("Apps WebServiceHandler","doInBackground");
         String response = "";
-        //establish connection
+        //establish connection "http://jsonplaceholder.typicode.com/posts/1"
         try {
-            URL url = new URL("http://jsonplaceholder.typicode.com/posts/1");
+            URL url = new URL("http://192.168.1.135:8080/registrationRest");
             Log.d("Apps WebServiceHandler","doInBackground - 1");
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             Log.d("Apps WebServiceHandler","doInBackground - 2");
+            //connection.setRequestProperty("Authorization", header);
             connection.setConnectTimeout(10000);
+            //allow to send datas
+                //connection.setDoOutput(true);
+            //set request property type
+            Log.d("Apps WebServiceHandler","doInBackground - 3");
+            //set request method
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            //create objects to sending datas
+            JSONObject data = new JSONObject();
+            data.put("username", "jakisUser");
+            data.put("password", "jakisPassword");
+            //send object
+            Log.d("Apps WebServiceHandler","doInBackground - 4");
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
+            writer.write(data.toString());
+            writer.close();
             connection.connect();
+            Log.d("Apps WebServiceHandler","doInBackground - 5: " + connection.getResponseCode());
             if (connection.getResponseCode() != 200) {
                 return null;
             }
             InputStream in = new BufferedInputStream(connection.getInputStream());
             response = streamToString(in);
-            Log.d("Apps WebServiceHandler","Response code: " + connection.getResponseCode());
+            Log.d("Apps WebServiceHandler","Response code: " + connection.getResponseCode() + " resp:" + response);
             connection.disconnect();
         } catch (Exception e) {
             Log.d("Apps WebServiceHandler","Exception has appeared: " + e);
