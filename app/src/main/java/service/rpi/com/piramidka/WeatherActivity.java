70dc4885259fb474ce4a6c3afc130fbba994bd01
Pawel <blachut.pawel@gmail.com>
2018-11-06 20:04:32 +0100

    Add functionality in Weather activity

--- app/src/main/java/service/rpi/com/piramidka/WeatherActivity.java
+++ app/src/main/java/service/rpi/com/piramidka/WeatherActivity.java
@@ -12,12 +12,26 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 import service.rpi.com.piramidka.webservice.WebServiceConnector;
 
 public class WeatherActivity extends AppCompatActivity {
 
     private Menu menu;
+    private TextView inTemp;
+    private TextView inHum;
+    private TextView outTemp;
+    private TextView outHum;
+    private TextView pressure;
+    private TextView measTime;
+    private TextView measDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +45,15 @@ public class WeatherActivity extends AppCompatActivity {
         //add up icon to Action Bar
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
+
+        //activity controls
+        inTemp = findViewById(R.id.inTempTextView);
+        inHum = findViewById(R.id.inHumTextView);
+        outTemp = findViewById(R.id.outTempTextView);
+        outHum = findViewById(R.id.outHumTextView);
+        pressure = findViewById(R.id.pressureTextView);
+        measTime = findViewById(R.id.timeValueTextView);
+        measDate = findViewById(R.id.dateValueTextView);
     }
 
     @Override
@@ -41,7 +64,7 @@ public class WeatherActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         menu.findItem(R.id.check_connection).setIcon(R.drawable.ic_sync);
-        new WebServiceConnector(WeatherActivity.this, menu, WebServiceConnector.AUTH).execute("weatherRest");
+        updateMeasuredValues();
         return true;
     }
 
@@ -51,7 +74,7 @@ public class WeatherActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             /** Connection button on ActionBar*/
             case R.id.check_connection:
-                new WebServiceConnector(WeatherActivity.this, menu, WebServiceConnector.AUTH).execute("weatherRest");
+                updateMeasuredValues();
                 break;
     }
         return super.onOptionsItemSelected(item);
@@ -63,6 +86,34 @@ public class WeatherActivity extends AppCompatActivity {
             // Inflate the layout for this fragment
             Log.d("Apps Main", "Create FRAGMENT.");
             return inflater.inflate(R.layout.weather_fragment, container, false);
+        }
+    }
+
+    private void updateMeasuredValues() {
+        String datas = "";
+        try {
+           datas = new WebServiceConnector(WeatherActivity.this, menu, WebServiceConnector.AUTH)
+                    .execute("weatherRest").get().get(1);
+        } catch (InterruptedException | ExecutionException e) {
+            Log.w("Apps Weather", "An exception has occured during getting measurements: " + e);
+            Toast.makeText(WeatherActivity.this, "Wystąpił problem podczas odczytu danych.", Toast.LENGTH_LONG).show();
+        }
+        Log.d("Apps Weather", "Update data measured for: " + datas);
+        try {
+            JSONObject json = new JSONObject(datas);
+            JSONObject inSens = json.getJSONObject("inSensor");
+            JSONObject outSens = json.getJSONObject("outSensor");
+            inTemp.setText(inSens.optString("temperature"));
+            inHum.setText(inSens.optString("humidity"));
+            pressure.setText(inSens.optString("pressure"));
+            outTemp.setText(outSens.optString("temperature"));
+            outHum.setText(outSens.optString("humidity"));
+            measDate.setText(inSens.optString("date").substring(5, 10));
+            measTime.setText(inSens.optString("date").substring(11, 16));
+            //2018-11-06 18:19:57
+        } catch (JSONException e) {
+            Log.w("Apps Weather", "An exception has occured during JSON conversion: " + e);
+            Toast.makeText(WeatherActivity.this, "Wystąpił problem podczas konwersji danych.", Toast.LENGTH_LONG).show();
         }
     }
 }
